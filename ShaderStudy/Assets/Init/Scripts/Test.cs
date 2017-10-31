063d33a30616ca9a1101b4dcdc94a210c1e09eeb
Stone <827443469@qq.com>
2017-10-31 21:34:05 +0800

    重温了顶点、片元着色器的基本结构

---
+++ ShaderStudy/Assets/Init/Scripts/Test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public Vector3 Vec;
+
+    [ContextMenu("VecT")]
+    public void VecT() {
+        Debug.Log(Vec.normalized);
+    }
+}
